979f288e7f01aa6cd2acbc5b509d61d22d400bf4
VISHAL KUMAR GUPTA <65849036+vkg2000@users.noreply.github.com>
2020-08-13 13:25:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+draw(getarr(slider.value),slider.value);
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  var arr = getarr(this.value);
+  draw(arr,this.value);
+}
+
+
+
+function getarr(length)
+{
+	var arr_random = new Array();
+	var arr_range = 500;
+	for (var i=0; i<length; i++) {
+    	arr_random.push(Math.round(Math.random() * arr_range));
+    }
+    return arr_random;
+    
+}
+
+function draw(arr,size)
+{
+    var div = document.getElementById("target-id");
+    
+    var html = "";
+    for (var i=0;i<size;i++) {
+        html += "<div class= 'bar'style = 'height: "+arr[i] +"px;'></div> "+ "<br>";
+
+    div.innerHTML = html;
+}
+}
+
+
+
+
+
