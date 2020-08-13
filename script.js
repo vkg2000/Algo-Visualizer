b00f215be815d1f9a61fc6dca899276e1eb30b0c
Akarsh Rana <vanshrana2012@gmail.com>
2020-08-13 15:48:11 +0530

    arr is global

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var slider = document.getElementById("myRange");
-draw(getarr(slider.value), slider.value);
+draw(getArray(slider.value), slider.value);
+
+arr = [];
 
 slider.oninput = function () {
-    var arr = getarr(this.value);
-    draw(arr, this.value);
+    getArray(this.value);
+    draw();
 }
-
-
 
 function getArray(length) {
     var arr_random = new Array();
@@ -14,21 +14,24 @@ function getArray(length) {
     for (var i = 0; i < length; i++) {
         arr_random.push(Math.round(Math.random() * arr_range));
     }
-    return arr_random;
-
+    arr = arr_random;
 }
 
-function draw(arr, size) {
+function draw() {
     var div = document.getElementById("target-id");
     var wid = 1100 / arr.length - 1;
     var html = "";
-    for (var i = 0; i < size; i++) {
+    for (var i = 0; i < arr.length; i++) {
         html += "<div class= 'bar'style = 'height: " + arr[i] + "px; width: " + wid + "px;'></div> " + "<br>";
-
         div.innerHTML = html;
     }
 }
 
 document.getElementById("reset").onclick = function () {
-    draw(getArray(50), 50);
+    getArray(50);
+    draw();
+}
+
+document.getElementById("mergeSort").onclick = function () {
+    mergeSort;
 }
